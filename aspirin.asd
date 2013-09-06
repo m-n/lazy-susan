e44eb962b5ddab11eab55369ffe48abe35c4d4f1
Matt Niemeir <matt.niemeir@gmail.com>
2013-09-05 20:01:06 -0500

    First swing at tests

--- aspirin.asd
+++ aspirin.asd
@@ -9,3 +9,14 @@
   :components ((:file "package")
                (:file "aspirin")))
 
+
+(asdf:defsystem #:aspirin-test
+  :serial t
+  :depends-on (:aspirin)
+  :components ((:file tests)))
+
+(defmethod asdf:perform ((o asdf:test-op)
+                         (c (eql (asdf:find-system :aspirin))))
+  (asdf:operate 'load-op :aspirin-test)
+  (funcall (intern (symbol-name :run-tests)
+                   (find-package :aspirin-test))))
---
+++ tests.lisp
@@ -0,0 +1,40 @@
+(defpackage #:aspirin-test
+  (:use #:cl #:aspirin)
+  (:shadowing-import-from #:aspirin
+                          #:looks-like-a-number))
+
+(in-package #:aspirin-test)
+
+(defvar *tests* ())
+
+(defun run-tests ()
+  (every #'identity
+         (mapcar (lambda (test)
+                   (format *trace-output* "~&Testing ~A." test)
+                   (if (funcall test)
+                       (progn (format *trace-output* " Passed.") t)
+                       (format *trace-output* " FAILED")))
+                 *tests*)))
+
+(defmacro deftest (name &body body)
+  `(progn (pushnew ',name *tests*)
+          (defun ,name () (and ,@body))))
+
+(defun sllan (s)
+  (looks-like-a-number (make-string-input-stream s 1)
+                                (schar s 0)))
+
+(deftest looks-like-a-number-1
+  (sllan "1")
+  (sllan "1.")
+  (sllan "11.1")
+  (sllan "1,000,000")
+  (sllan "10e6")
+  (sllan "1d0")
+  (sllan "1/2")
+  (sllan "11/22"))
+
+(deftest doesn't-look-numberlike
+  (not (sllan "a1"))
+  (not (sllan "abc"))
+  (not (sllan "1/")))
